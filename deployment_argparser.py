cff76940085bc054006e0d6ad9e0e7212d17b835
jack wolf - optcluster <jwolf2@optcluster>
2021-01-27 18:31:35 -0600

    work

--- deployment_argparser.py
+++ deployment_argparser.py
@@ -1,14 +1,7 @@
 import argparse
+from distutils.util import strtobool
 
-def str2bool(v):
-    if isinstance(v, bool):
-        return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
+str2bool = lambda x: bool(strtobool(x))
 
 def deployment_parser():
     parser = argparse.ArgumentParser()
